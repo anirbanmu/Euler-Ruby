9447a737c6beaac6a6508704a14f889a66d2c3ef
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2017-04-22 22:19:46 -0400

    Problem 1 solution

--- euler.rb
+++ euler.rb
@@ -1,12 +1,43 @@
 #!/usr/bin/env ruby
 
+require 'benchmark'
+
+class TimedProblemSolution
+    attr_accessor :idx, :result, :time
+
+    def initialize(idx, result, time)
+        @idx = idx
+        @result = result
+        @time = time
+    end
+end
+
+def time_problem_solve(idx, problem)
+    result = nil
+    realtime = Benchmark.realtime { result = problem.solve }
+    return TimedProblemSolution.new(idx, result, realtime * 1000 * 1000)
+end
+
 def run_euler_problems()
+    problems = []
     Dir["./problems/*.rb"].each do |problem_file|
         require problem_file
         problem_number = File.basename(problem_file, '.rb')
-        problem_class_name = Object.const_get('Problem' + problem_number)
-        problem = problem_class_name.new
-        puts 'Problem ' + problem_number + ": " + problem.solve.to_s
+        problems.push(time_problem_solve(problem_number.to_i, Object.const_get('Problem' + problem_number).new))
+    end
+
+    problems.sort do |a, b|
+        if (a < b)
+            -1
+        end
+        if (b > a)
+            1
+        end
+        0
+    end
+
+    problems.each do |p|
+        puts 'Problem ' + p.idx.to_s + ': ' + p.result.to_s + " ( " + p.time.to_s + " μs )"
     end
 end
 
--- problems/1.rb
+++ problems/1.rb
@@ -1,12 +1,19 @@
 require './problem_interface.rb'
 
+def accumulate_multiples(limit, factor, ignore_factor = nil)
+    sum = 0
+    n = factor
+    while n < limit
+        if ignore_factor == nil or n % ignore_factor != 0
+            sum += n
+        end
+        n += factor
+    end
+    return sum
+end
+
 class Problem1 < ProblemBase
     def solve
-        @result = 1
-        return solve1
-    end
-
-    def solve1
-        return @result
+        accumulate_multiples(1000, 3) + accumulate_multiples(1000, 5, 3)
     end
 end
